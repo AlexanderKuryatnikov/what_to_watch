097c1ecd856a250c6128899435c4287457e46468
Alexander Kuryatnikov <asander1995@gmail.com>
2022-09-24 03:12:58 +0300

    added model to db

--- opinions_app.py
+++ opinions_app.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from random import randrange
 
 app = Flask(__name__)
 
@@ -9,9 +11,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+class Opinion(db.Model):
+    # ID — целое число, первичный ключ
+    id = db.Column(db.Integer, primary_key=True)
+    # Название фильма — строка длиной 128 символов, не может быть пустым
+    title = db.Column(db.String(128), nullable=False)
+    # Мнение о фильме — большая строка, не может быть пустым,
+    # должно быть уникальным
+    text = db.Column(db.Text, unique=True, nullable=False)
+    # Ссылка на сторонний источник — строка длиной 256 символов
+    source = db.Column(db.String(256))
+    # Дата и время — текущее время,
+    # по этому столбцу база данных будет проиндексирована
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+
 @app.route('/')
 def index_view():
-    return 'Совсем скоро тут будет случайное мнение о фильме!'
+    # Определяется количество мнений в базе данных
+    quantity = Opinion.query.count()
+    # Если мнений нет,
+    if not quantity:
+        # то возвращается сообщение
+        return 'В базе данных мнений о фильмах нет.'
+    # Иначе выбирается случайное число в диапазоне от 0 и до quantity
+    offset_value = randrange(quantity)
+    # И определяется случайный объект
+    opinion = Opinion.query.offset(offset_value).first()
+    return opinion.text
 
 
 if __name__ == '__main__':
